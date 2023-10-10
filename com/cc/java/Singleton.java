f495118a1529aa4d608a9b8dd76eccb7e6baf7e2
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-10-10 09:06:39 +0000

    Objektbildung / Singleton

--- com/cc/java/Singleton.java
+++ com/cc/java/Singleton.java
@@ -2,8 +2,19 @@ package com.cc.java;
 
 public class Singleton {
   
+    /* Objektbildung */
+
+    private static Singleton instance;
    
+    private Singleton(){
+        Helper.ausgabe("Hi, I'm there for you!");
+    }
 
-
+    public static Singleton getInstance(){
+        if (instance == null) {
+          instance = new Singleton();  
+        }
+        return instance;
+    }
 
 }
